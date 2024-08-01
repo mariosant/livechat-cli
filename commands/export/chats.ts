79379f1820efb3bc8ad843a1ad9fd95bd5f8e246
Marios Antonoudiou <m.antonoudiou@celonis.com>
2024-08-02 00:02:37 +0300

    Add chat foratters

--- commands/export/chats.ts
+++ commands/export/chats.ts
@@ -5,10 +5,29 @@ import PQueue from "p-queue";
 import JsonURL from "@jsonurl/jsonurl";
 import { resolve } from "path";
 import { agentApiClient } from "../../utils/livechat-api-client";
+import type { ChatData } from "../../types";
 
 const queue = new PQueue({
   autoStart: true,
 });
+
+const jsonFormatter = (chat: ChatData) => JSON.stringify(chat, null, 4);
+
+const plainConvoFormatter = (chat: ChatData) => {
+  const events = get<ChatData["thread"]["events"]>(chat, "thread.events", []);
+
+  const content = events
+    .filter((event) => event.type === "message")
+    .filter((event) => event.visibility === "all")
+    .reduce((output, event) => {
+      const authorType = event.author_id?.includes("@") ? "Agent" : "Visitor";
+      const line = `${authorType}: ${event.text}\n`;
+
+      return `${output}${line}`;
+    }, "");
+
+  return content;
+};
 
 export const exportChatsCommand = defineCommand({
   meta: {
@@ -29,21 +48,32 @@ export const exportChatsCommand = defineCommand({
         "Pass request filters as jsonurl format. See more at https://platform.text.com/docs/messaging/agent-chat-api#list-archives",
       type: "string",
     },
+    view: {
+      default: "json",
+      valueHint: "json | plain-convo",
+      description: "Pass the output format of your choice.",
+      type: "string",
+    },
   },
   run: async (context) => {
     consola.start("Starting chats export");
 
-    const filters = JsonURL.parse(context.args.filters);
+    const filters = JsonURL.parse(context.args.filters ?? "");
 
-    const writeChatToDisk = async (chat: unknown) => {
+    const writeChatToDisk = async (chat: ChatData) => {
       const chatId = get(chat, "id");
       const threadId = get(chat, "thread.id");
+      const fileContent =
+        context.args.view === "plain-convo"
+          ? plainConvoFormatter(chat)
+          : jsonFormatter(chat);
+
+      const fileExt = context.args.view === "plain-convo" ? "txt" : "json";
 
       const filename = resolve(
         String(context.args.directory),
-        `${chatId}-${threadId}.json`
+        `${chatId}-${threadId}.${fileExt}`
       );
-      const fileContent = JSON.stringify(chat, null, 4);
 
       consola.debug(`Writing chat to disk`, { chatId, threadId, filename });
       await Bun.write(filename, fileContent);
@@ -60,7 +90,7 @@ export const exportChatsCommand = defineCommand({
       });
 
       const nextPageId = get<string | undefined>(response, "next_page_id");
-      const chats = get<Record<string, unknown>[]>(response, "chats", []);
+      const chats = get<ChatData[]>(response, "chats", []);
 
       queue.addAll(chats.map((chat) => () => writeChatToDisk(chat)));
 
---
+++ types.ts
@@ -0,0 +1,113 @@
+export type ChatData = {
+  id: string;
+  users: {
+    id: string;
+    name: string;
+    email: string;
+    events_seen_up_to: string;
+    type: string;
+    present: boolean;
+    created_at?: string;
+    last_visit?: {
+      id: number;
+      started_at: string;
+      ended_at: string;
+      ip: string;
+      user_agent: string;
+      geolocation: {
+        country: string;
+        country_code: string;
+        region: string;
+        city: string;
+        timezone: string;
+        latitude: string;
+        longitude: string;
+      };
+      last_pages: {
+        opened_at: string;
+        url: string;
+        title: string;
+      }[];
+      previous_visit_started_at: string;
+    };
+    statistics?: {
+      chats_count: number;
+      threads_count: number;
+      visits_count: number;
+      page_views_count: number;
+      greetings_shown_count: number;
+      greetings_accepted_count: number;
+    };
+    agent_last_event_created_at?: string;
+    customer_last_event_created_at?: string;
+    email_verified?: boolean;
+    avatar?: string;
+    visibility?: string;
+  }[];
+  thread: {
+    id: string;
+    active: boolean;
+    user_ids: string[];
+    properties: {
+      routing: {
+        continuous: boolean;
+        idle?: boolean;
+        referrer?: string;
+        start_url?: string;
+        unassigned?: boolean;
+        pinned?: boolean;
+        was_pinned?: boolean;
+      };
+      source: {
+        client_id: string;
+        customer_client_id?: string;
+      };
+    };
+    access: {
+      group_ids: number[];
+    };
+    previous_thread_id: string;
+    next_thread_id: string;
+    previous_accessible_thread_id: string;
+    next_accessible_thread_id: string;
+    created_at: string;
+    events: {
+      id: string;
+      created_at: string;
+      visibility: string;
+      type: string;
+      properties?: {
+        source: {
+          client_id: string;
+        };
+      };
+      text?: string;
+      author_id?: string;
+      custom_id?: string;
+      system_message_type?: string;
+      text_vars?: {
+        agent_added?: string;
+        agent_removed?: string;
+        agent?: string;
+      };
+    }[];
+  };
+  properties: {
+    routing: {
+      continuous: boolean;
+      pinned?: boolean;
+      was_pinned?: boolean;
+    };
+    source: {
+      client_id: string;
+      customer_client_id?: string;
+    };
+    supervising?: {
+      agent_ids: string;
+    };
+  };
+  access: {
+    group_ids: number[];
+  };
+  is_followed: boolean;
+};
